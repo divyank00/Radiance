815a17e48a081c36d8a520d8164549c2693c177d
divyank00 <lunkaddivyank@gmail.com>
2020-02-26 23:28:31 +0530

    first Commit

--- app/src/main/java/com/pascw/radianceregistration/Config.java
+++ app/src/main/java/com/pascw/radianceregistration/Config.java
@@ -3,5 +3,5 @@ package com.pascw.radianceregistration;
 public class Config {
 
     public static final String EMAIL ="pasc.radiance@gmail.com"; //your-gmail-username   acm.pict@gmail.com
-    public static final String PASSWORD ="radiance1@34";
+    public static final String PASSWORD ="pascw@1415";
 }
